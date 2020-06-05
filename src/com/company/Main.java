b74e14444ba5b585dfb5bd5d92d0ab7912d67733
harsh <harshrana2101@gmail.com>
2020-06-05 12:45:02 -0400

    5.0 increment and decrement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,9 +17,14 @@ public class Main {
 
         /*to calculate interest on $10,000 with interst rate 2,3,4,5,6,7,8.
         and to print results in console*/
+        System.out.println("to print interset rate from 2% to 8% ");
 
         for (int i = 2; i < 9; i++) {
-            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + (calculateInterest(10000, i)));
+            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + String.format("%.2f",calculateInterest(10000, i)));
+        }
+        System.out.println("to print interset rate from 8% to 2% ");
+        for (int i = 8; i > 1; i--) {
+            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + String.format("%.2f",calculateInterest(10000, i)));
         }
     }
 
