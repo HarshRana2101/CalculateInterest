b4c62e66d6829e90c9806727b9bb89129f3e6788
harsh <harshrana2101@gmail.com>
2020-06-05 13:01:10 -0400

    6.0 increment and decrement with prime number check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,18 +17,45 @@ public class Main {
 
         /*to calculate interest on $10,000 with interst rate 2,3,4,5,6,7,8.
         and to print results in console*/
+
         System.out.println("to print interset rate from 2% to 8% ");
 
         for (int i = 2; i < 9; i++) {
-            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + String.format("%.2f",calculateInterest(10000, i)));
+            System.out.println("interest on $10,000 with interest rate " +
+                    i + " is = $" + String.format("%.2f", calculateInterest(10000, i)));
         }
         System.out.println("to print interset rate from 8% to 2% ");
         for (int i = 8; i > 1; i--) {
-            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + String.format("%.2f",calculateInterest(10000, i)));
+            System.out.println("interest on $10,000 with interest rate " +
+                    i + " is = $" + String.format("%.2f", calculateInterest(10000, i)));
+        }
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime Number");
+                if (count == 10) {
+                    System.out.println("exiting the loop");
+                    break;
+                }
+            }
         }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
+    }
+
+    /*************************************************************************************************************************/
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
