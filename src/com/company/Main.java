902d8a82493ec8c220c4c14efdf4f5dc58045525
harsh <harshrana2101@gmail.com>
2020-06-05 12:39:26 -0400

    4.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,18 @@ public class Main {
         System.out.println
                 ("final price with interest is " + calculateInterest(39900.00, 6.99));
 
-
         /*for (int i = 0; i < 5; i++) {
-            System.out.println("loop" + i + "= hello");
+            System.out.println("loop" + i + "= hello");}*/
+
+        /*to calculate interest on $10,000 with interst rate 2,3,4,5,6,7,8.
+        and to print results in console*/
+
+        for (int i = 2; i < 9; i++) {
+            System.out.println("interest on $10,000 with interest rate " + i + " is = $" + (calculateInterest(10000, i)));
         }
-*/
     }
 
     public static double calculateInterest(double amount, double interestRate) {
-        return (amount + amount * (interestRate / 100));
+        return (amount * (interestRate / 100));
     }
 }
