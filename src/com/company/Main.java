bc2f955226a1fddf4db1f4883f14f914254d1ccc
harsh <harshrana2101@gmail.com>
2020-06-05 12:31:50 -0400

    3.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println
-                ("final price with interest is " +
-                        calculateInterest(40000.00, 12));
+                ("final price with interest is " + calculateInterest(40000.00, 12));
         System.out.println
-                ("final price with interest is " +
-                        calculateInterest(10000.00, 2.0));
+                ("final price with interest is " + calculateInterest(10000.00, 2.0));
         System.out.println
-                ("final price with interest is " +
-                        calculateInterest(36999, 5.0));
+                ("final price with interest is " + calculateInterest(36999, 5.0));
         System.out.println
-                ("final price with interest is " +
-                        calculateInterest(39900.00, 6.99));
+                ("final price with interest is " + calculateInterest(39900.00, 6.99));
+
+
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println("loop" + i + "= hello");
+        }
+*/
     }
 
     public static double calculateInterest(double amount, double interestRate) {
